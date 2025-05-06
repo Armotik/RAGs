b88d537d4192ba9c151319ab59c9870d97da79ee
Your Name <anthonymudet94@gmail.com>
2025-05-06 15:35:02 +0200

    v1.20.24

--- benchmarks/preprocessing/NER_benchmark.py
+++ benchmarks/preprocessing/NER_benchmark.py
@@ -361,9 +361,9 @@ def run_transformers(model_name, text):
 def process_model(model_id, framework, text, model_name):
     start = time()
     if framework == "transformers":
-        entities = run_transformers(model_name, text)
+        entities = run_transformers(model_id, text)
     else:
-        entities = run_spacy(model_name, text)
+        entities = run_spacy(model_id, text)
     duration = time() - start
 
     tfidf = compute_tfidf(entities)
